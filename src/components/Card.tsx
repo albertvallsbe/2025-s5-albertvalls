69e09b1d46b9b3a0c3f0c10fb862855deb6f3d1d
Albert <avalls89@gmail.com>
2025-06-30 22:46:10 +0200

    Add random image from API randomFox to pic a image in every card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import type { CardProps } from "../types/types";
+import { RandomImage } from "./RandomImage";
 
 export const Card = ({ card, controls }: CardProps): JSX.Element => {
   const { stepIndex, totalSteps, isFirst, isLast, next, prev, goTo } = controls;
@@ -6,9 +7,10 @@ export const Card = ({ card, controls }: CardProps): JSX.Element => {
 
   return (
     <article style={{ background: card.bgColor }}>
+      <RandomImage key={stepIndex} />
       <h2>{card.title}</h2>
       <p>{card.description}</p>
-      {card.image && <img src={card.image} alt={card.title} />}
+      {/* {card.image && <img src={card.image} alt={card.title} />} */}
 
       <div className="navigation">
         {!isFirst && <button onClick={prev}>←</button>}
