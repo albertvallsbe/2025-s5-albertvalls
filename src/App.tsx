51cca4ba8cba92750c9179154e24774e186de355
Albert <avalls89@gmail.com>
2025-07-01 17:48:19 +0200

    Update tsconfig to deploy in netlify

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import type { TutorialStep } from "./types/types";
 import dataCards from "./data/dataCards.json";
 import { Card } from "./components/Card";
 import { useNavigation } from "./hooks/useNavigation";
-// import "./App.css";
 
 const App = (): JSX.Element => {
   const tutorialData: TutorialStep[] = dataCards;
